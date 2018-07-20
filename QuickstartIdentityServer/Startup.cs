cf0dfa95be06835dc1840a72c7def95123403d1c
FQLin <fanqinglin1990@hotmail.com>
2018-07-20 18:40:13 +0800

    Add files via upload

---
+++ QuickstartIdentityServer/Startup.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace QuickstartIdentityServer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            // 使用内存存储，密钥，客户端和资源来配置身份服务器。
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients())
+                .AddTestUsers(Config.GetUsers())
+                .AddInMemoryIdentityResources(Config.GetIdentityResources());
+
+            //services.AddAuthentication()
+            //    .AddGoogle("Google", options =>
+            //    {
+            //        options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+            //        options.ClientId = "407100308766-eoeqp83irrcd8pndqhg00ega7dufk44l.apps.googleusercontent.com";
+            //        options.ClientSecret = "T3tNAd65zaY7njnd-ziCX7n1";
+            //    })
+            //.AddOpenIdConnect("oidc", "OpenId Connect", options =>
+            //     {
+            //         options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+            //         options.SignOutScheme = IdentityServerConstants.SignoutScheme;
+
+            //         options.Authority = ""
+
+            //     })
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
